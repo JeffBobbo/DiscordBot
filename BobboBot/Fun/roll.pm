6d78581bc7e74e1b2cbc0e1c333062d3d0ec056d
JeffBobbo <james.delph@hotmail.co.uk>
2017-06-10 00:40:59 +0100

    Rewritten roll to allow dnd style rolls

--- BobboBot/Fun/roll.pm
+++ BobboBot/Fun/roll.pm
@@ -16,28 +16,54 @@ sub run
   my $argv = $hash->{argv};
   return help() if (!defined $argv || index($argv, '-h') != -1);
 
-  if ($argv !~ /^[\d,]+$/)
+  if ($argv =~ /^(?:([\d,]+)d)?([\d,]+)$/)
   {
-    return 'I can\'t roll on that.';
+    my $n = defined $1 ? $1 : 1;
+    my $sides = defined $2 ? $2 : 1;
+    $n =~ s/,//g;
+    $sides =~ s/,//g;
+
+    return 'Dice come with at least two sides.' if ($sides < 2);
+    return 'Can\'t roll less than once.' if ($n < 1);
+    return 'Can\'t roll more than six.' if ($n > 6);
+
+    my @rolls;
+    my $sum = 0;
+    my $max = $n * $sides;
+    for (1..$n)
+    {
+      my $x = random(1, $sides);
+      $sum += $x;
+      push(@rolls, $x);
+    }
+
+    my $ret = "<\@$hash->{author}{id}> rolled " . commify($sum);
+    $ret .= " (" . join(', ', @rolls) . ")" if ($n > 1);
+    $ret .= " out of $max.";
+    return $ret;
   }
-
-  $argv =~ s/,//g;
-
-  if ($argv < 2)
-  {
-    return 'Can\'t roll on less than a two.';
-  }
-
-  my $r = commify(random(1, $argv));
-  $argv = commify($argv);
-  return "<\@$hash->{author}{id}> rolled $r out of $argv.";
+  return  "Unrecognised rolling style, see `$main::config->{prefix}roll -h`.";
 }
 
 sub help
 {
-  return $main::config->{prefix} . 'roll (max) - Roll a random number from 1 to max';
+  return <<EOF
+```
+$main::config->{prefix}roll [Nd]S
+
+Rolls a uniformly distributed dice.
+
+[Nd]S:
+  Roll an S sided dice, optionally N times. When supplied, N can't be greater than 6.
+  $main::config->{prefix}roll 6 -- Rolls a 6 sided dice.
+  $main::config->{prefix}roll 5d20 -- Rolls a 20 sided dice 5 times.
+```
+EOF
 }
 
-BobboBot::Core::module::addCommand('roll', \&BobboBot::Fun::roll::run);
+if ($INC{'BobboBot/Core/event.pm'})
+{
+  BobboBot::Core::module::addCommand('roll', \&BobboBot::Fun::roll::run);
+}
 
 1;
