d63eb73309b8cdeb1df74914299457591c59502b
JeffBobbo <JeffBobbo@users.noreply.github.com>
2018-01-14 19:44:49 +0000

    Lots of fixes and tweaks

--- BobboBot/StarSonata/bboard.pm
+++ BobboBot/StarSonata/bboard.pm
@@ -19,49 +19,62 @@ my $postid = '327847763351961601';
 
 sub bboard
 {
-  return XML::Simple->new->XMLin(get($source), KeepRoot => 1, ForceArray => ['ENTRY']);
+  my $xml = get($source);
+  return if (!defined $xml);
+  return XML::Simple->new->XMLin($xml, KeepRoot => 1, ForceArray => ['ENTRY']);
+}
+
+sub globals
+{
+  my $xml = get('http://starsonata.com/ss_api/globals.xml');
+  return if (!defined $xml);
+  return XML::Simple->new->XMLin($xml, KeepRoot => 1, SuppressEmpty => '');
 }
 
 sub post
 {
   $postid = shift() || $postid;
   my $bboard = bboard();
+  my $globals = globals();
 
-  # do nothing if there's nothing to do
-  return if (!defined $bboard);
-
-  my $dt = DateTime->now(time_zone => 'America/New_York');
-  my $topost = "```Last updated: " . $dt->datetime(' ') . ' ' . ($dt->is_dst() ? 'EDT' : 'EST');
-
-  my @posts = sort {$b->{credits} <=> $a->{credits}} @{$bboard->{BBOARD}{ENTRY}};
-  while (@posts)
+  if (defined $bboard)
   {
-    my $old = $topost;
-    my $e = shift(@posts);
-    $topost = "$e->{notice}\n$e->{author} at " . commify($e->{credits}) . " credits\n" . ("=" x 10) . "\n" . $topost;
-    if (length($topost) > 2000)
+    my $dt = DateTime->now(time_zone => 'America/New_York');
+    my $topost = "```Last updated: " . $dt->datetime(' ') . ' ' . ($dt->is_dst() ? 'EDT' : 'EST');
+
+    my @posts = sort {$b->{credits} <=> $a->{credits}} @{$bboard->{BBOARD}{ENTRY}};
+    while (@posts)
     {
-      $topost = $old;
-      last;
+      my $old = $topost;
+      my $e = shift(@posts);
+      $topost = "$e->{notice}\n$e->{author} at " . commify(int($e->{credits})) . " credits\n" . ("=" x 10) . "\n" . $topost;
+      if (length($topost) > 2000)
+      {
+        $topost = $old;
+        last;
+      }
+    }
+    $topost = '```' . $topost;
+
+    if (defined $postid)
+    {
+      $main::discord->edit_message($postto, $postid, $topost);
+    }
+    else
+    {
+      $main::discord->send_message($postto, $topost);
     }
   }
-  $topost = '```' . $topost;
 
-  my $globals = XML::Simple->new->XMLin(get('http://starsonata.com/ss_api/globals.xml'), KeepRoot => 1, SuppressEmpty => '');
-  my $dawn = DateTime->from_epoch(epoch => $globals->{GLOBALS}{UNISTART}, time_zone => 'America/New_York');
-  my $topic = "Dawn of time: " . $dawn->datetime(' ') . ' ' . ($dawn->is_dst() ? 'EDT' : 'EST');
-  if ($globals->{GLOBALS}{LOGIN_MSG} ne '')
+  if (defined $globals)
   {
-    $topic .= ". Login message: " . $globals->{GLOBALS}{LOGIN_MSG};
-  }
-  $main::discord->set_topic($postto, $topic);
-  if (defined $postid)
-  {
-    $main::discord->edit_message($postto, $postid, $topost);
-  }
-  else
-  {
-    $main::discord->send_message($postto, $topost);
+    my $dawn = DateTime->from_epoch(epoch => $globals->{GLOBALS}{UNISTART}, time_zone => 'America/New_York');
+    my $topic = "Dawn of time: " . $dawn->datetime(' ') . ' ' . ($dawn->is_dst() ? 'EDT' : 'EST');
+    if ($globals->{GLOBALS}{LOGIN_MSG} ne '')
+    {
+      $topic .= ". Login message: " . $globals->{GLOBALS}{LOGIN_MSG};
+    }
+    $main::discord->set_topic($postto, $topic);
   }
   return;
 }
